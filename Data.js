9b9fd09a405089c08248068270be745bcef5e2b1
OtavioLRSo <otavioleitesantos@hotmail.com>
2021-05-16 01:22:14 -0300

    data.js useful?

--- Data.js
+++ Data.js
@@ -123,7 +123,3 @@ function structureHorizonData(data) {
 const d3 = require('d3');
 
 const parseDate = d3.timeParse('%m/%Y');
-
-function teste() {
-  console.log('sim')
-}
--- Database.js
+++ Database.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-const { structureExportData } = require('./Data');
 
 const connection = mysql.createPool({
   host: 'localhost',
