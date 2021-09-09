130422b1eb93e492e2a2682c56a495fb4df891d5
OtavioLRSo <otavioleitesantos@hotmail.com>
2021-09-09 20:09:35 -0300

    print

--- src/index.js
+++ src/index.js
@@ -14,5 +14,10 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Servidor inicializado na porta ${PORT}`);
-  console.log(process.env)
+  console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_NAME,
+    password: process.env.DB_USER,
+    database: process.env.DB_PASS
+  })
 });
